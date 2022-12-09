6d478fc11557366c7bd9cdcf0cf1fb36e6f62844
william-kies <william.kies04@gmail.com>
2022-12-09 11:35:08 +0100

    utilisation de [ngClass] pour changer l'affichage selon un évènement via une condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,7 @@ export class AppComponent implements OnInit {
         description: 'Astronaute nageant dans un océan de couleurs.',
         imageUrl: 'https://external-preview.redd.it/XrrqA4tXSxpfTl8M2AGNssZpks3MJwQ2LuDzNnsam64.png?format=pjpg&auto=webp&s=4c016353dcadabee95600f4bb833ce138b02894d',
         createDate: new Date(),
-        likes: 389,
+        likes: 0,
         location: 'Dans l\'espace'
       },
       {
@@ -24,14 +24,14 @@ export class AppComponent implements OnInit {
         description: 'Peinture d\'une forêt boréale, lieu méconnu en pleine nature.',
         imageUrl: 'https://i.redd.it/wrywjpghzgi11.png',
         createDate: new Date(),
-        likes: 86
+        likes: 0
       },
       {
         title: 'Nature dans une ampoule',
         description: 'Photo d\'un espace naturel présent seulement dans le bulbe d\'une ampoule.',
         imageUrl: 'https://moewalls.com/wp-content/uploads/2022/03/nature-in-a-lightbulb-thumb.jpg',
         createDate: new Date(),
-        likes: 47
+        likes: 0
       }
     ];
   }
