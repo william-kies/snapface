46c3f8be22a8b33ec338f92744bb6bf6e56568c7
william-kies <william.kies04@gmail.com>
2022-12-14 14:49:28 +0100

    ajout de validation aux contrôles des formulaires en passant des Validators au moment de la configuration du FormGroup, Validators.required rend un champ requis et .pattern verifie grâce à du regEx que le contenu du champs correspond bien à une expression régulière

--- src/app/new-face-snap/new-face-snap.component.ts
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Observable, map } from 'rxjs';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { FaceSnap } from '../models/face-snap.model';
+import { map } from 'rxjs/operators';
+import { FaceSnapsService } from '../services/face-snaps.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-face-snap',
@@ -11,17 +14,22 @@ import { FaceSnap } from '../models/face-snap.model';
 export class NewFaceSnapComponent implements OnInit {
   snapForm!: FormGroup;
   faceSnapPreview$!: Observable<FaceSnap>;
+  urlRegex!: RegExp;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private faceSnapService: FaceSnapsService, private router: Router) {
 
   }
 
   ngOnInit(): void {
+    this.urlRegex = /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.=#?&/=]*)/;
+
     this.snapForm = this.formBuilder.group({
-      title: [null],
-      description: [null],
-      imageUrl: [null],
+      title: [null, Validators.required],
+      description: [null, Validators.required],
+      imageUrl: [null, [Validators.required, Validators.pattern(this.urlRegex)]],
       location: [null]
+    }, {
+      updateOn: 'blur'
     });
     
     this.faceSnapPreview$ = this.snapForm.valueChanges.pipe(
@@ -35,6 +43,7 @@ export class NewFaceSnapComponent implements OnInit {
   }
 
   onSubmitForm(): void {
-    console.log(this.snapForm.value);
+    this.faceSnapService.addFaceSnap(this.snapForm.value);
+    this.router.navigateByUrl('snapface');
   }
 }
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -51,4 +51,15 @@ export class FaceSnapsService {
         const faceSnap = this.getFaceSnapById(faceSnapId);
         likeType === 'like' ? faceSnap.likes++ : faceSnap.likes--;
     }
+
+    addFaceSnap(formValue: {title: string, description: string, imageUrl: string, location?: string}): void {
+        const faceSnap: FaceSnap = {
+            ...formValue,
+            likes: 0,
+            createDate: new Date(),
+            id: this.faceSnaps[this.faceSnaps.length - 1].id + 1
+        }
+
+        this.faceSnaps.push(faceSnap);
+    }
 }
